4ad23b92015d0dfb04295ed2f31be8162e354e87
alexandrmefisto <verjil159753>
2022-06-14 20:18:26 +0300

    added field manager boolean

--- src/main/java/com/teamvoy/task/entity/UserEntity.java
+++ src/main/java/com/teamvoy/task/entity/UserEntity.java
@@ -8,11 +8,19 @@ public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    private String userName;
+    private String username;
     private String password;
-    private boolean manager;
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
     private List<OrderEntity> orders;
+    private Boolean manager;
+
+    public Boolean isManager() {
+        return manager;
+    }
+
+    public void setManager(Boolean manager) {
+        this.manager = manager;
+    }
 
     public UserEntity() {
     }
@@ -33,12 +41,12 @@ public class UserEntity {
         this.id = id;
     }
 
-    public String getUserName() {
-        return userName;
+    public String getUsername() {
+        return username;
     }
 
-    public void setUserName(String userName) {
-        this.userName = userName;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getPassword() {
