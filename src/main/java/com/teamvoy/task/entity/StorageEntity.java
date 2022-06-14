33df5820701e135e4172ccc1847d6607308859e2
alexandrmefisto <verjil159753>
2022-06-14 21:53:56 +0300

    added  getters and setters and constructor

--- src/main/java/com/teamvoy/task/entity/StorageEntity.java
+++ src/main/java/com/teamvoy/task/entity/StorageEntity.java
@@ -14,5 +14,40 @@ public class StorageEntity {
     @JoinColumn(name = "phone_id")
     private PhoneEntity phone;
     private Long count;
+
+    public StorageEntity() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public OrderEntity getOrder() {
+        return order;
+    }
+
+    public void setOrder(OrderEntity order) {
+        this.order = order;
+    }
+
+    public PhoneEntity getPhone() {
+        return phone;
+    }
+
+    public void setPhone(PhoneEntity phone) {
+        this.phone = phone;
+    }
+
+    public Long getCount() {
+        return count;
+    }
+
+    public void setCount(Long count) {
+        this.count = count;
+    }
 }
 
