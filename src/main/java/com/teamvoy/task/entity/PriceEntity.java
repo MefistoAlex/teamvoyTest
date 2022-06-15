b7490173f3d784c82866fb0048490b88cf88b4e6
alexandrmefisto <verjil159753>
2022-06-15 21:57:05 +0300

    added constructor and getter and setter

--- src/main/java/com/teamvoy/task/entity/PriceEntity.java
+++ src/main/java/com/teamvoy/task/entity/PriceEntity.java
@@ -12,4 +12,31 @@ public class PriceEntity {
     @JoinColumn(name = "phone_id")
     private PhoneEntity phone;
     private Double price;
+
+    public PriceEntity() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public PhoneEntity getPhone() {
+        return phone;
+    }
+
+    public void setPhone(PhoneEntity phone) {
+        this.phone = phone;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
 }
