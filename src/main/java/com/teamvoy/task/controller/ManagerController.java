42693d92a6ed061d20b55a09455acb524fa1a663
alexandrmefisto <verjil159753>
2022-06-15 19:46:36 +0300

    added method to get rest of phones

--- src/main/java/com/teamvoy/task/controller/ManagerController.java
+++ src/main/java/com/teamvoy/task/controller/ManagerController.java
@@ -1,27 +1,40 @@
 package com.teamvoy.task.controller;
 
 import com.teamvoy.task.entity.PhoneEntity;
-import com.teamvoy.task.entity.UserEntity;
-import com.teamvoy.task.exception.UserAlreadyExistException;
+import com.teamvoy.task.model.Order;
 import com.teamvoy.task.service.ManagerService;
-import com.teamvoy.task.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/manager")
 public class ManagerController {
     @Autowired
     private ManagerService managerService;
-    @PostMapping
+    @PostMapping("/add_phone")
     public ResponseEntity addPhone(@RequestBody PhoneEntity phone) {
         try {
             managerService.addPhone(phone);
             return ResponseEntity.ok().body("Phone added successfully");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+    @PostMapping("/order")
+    public ResponseEntity incomingOrder(@RequestBody Order order) {
+        try {
+            managerService.incomingOrder(order);
+            return ResponseEntity.ok().body("Incoming order added successfully");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+    @GetMapping("/storage")
+    public ResponseEntity getStorageStock(){
+        try {
+            managerService.getStorageStock();
+            return ResponseEntity.ok().body(managerService.getStorageStock());
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
--- src/main/java/com/teamvoy/task/service/ManagerService.java
+++ src/main/java/com/teamvoy/task/service/ManagerService.java
@@ -1,16 +1,77 @@
 package com.teamvoy.task.service;
 
+import com.teamvoy.task.entity.OrderEntity;
 import com.teamvoy.task.entity.PhoneEntity;
+import com.teamvoy.task.entity.StorageEntity;
+import com.teamvoy.task.model.Order;
+import com.teamvoy.task.model.OrderLine;
+import com.teamvoy.task.model.StorageLine;
+import com.teamvoy.task.repository.OrderRepository;
 import com.teamvoy.task.repository.PhoneRepository;
+import com.teamvoy.task.repository.StorageRepository;
+import com.teamvoy.task.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ManagerService {
-        @Autowired
-        private PhoneRepository phoneRepository;
-        public PhoneEntity addPhone(PhoneEntity phone){
-            return phoneRepository.save(phone);
+    @Autowired
+    private PhoneRepository phoneRepository;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private OrderRepository orderRepository;
+    @Autowired
+    private StorageRepository storageRepository;
+
+    public PhoneEntity addPhone(PhoneEntity phone) {
+        //adding phone to DB
+        return phoneRepository.save(phone);
+    }
+
+    public OrderEntity incomingOrder(Order order) {
+        //test order
+        /* order.setIncoming(true);
+        order.setUser(User.toModel(userRepository.findById(2L).get()));
+        OrderLine orderLine = new OrderLine(phoneRepository.findById(3L).get(),4L,4.12D );
+        OrderLine orderLine2 = new OrderLine(phoneRepository.findById(4L).get(),13L,23.12D );
+        order.getOrderList().add(orderLine);
+        order.getOrderList().add(orderLine2);*/
+        OrderEntity entity = toEntityOrder(order);
+        return orderRepository.save(entity);
+    }
+
+    public OrderEntity toEntityOrder(Order order) {
+        //convert OrderModel to OrderEntity
+        OrderEntity entity = new OrderEntity();
+        entity.setUser(userRepository.findById(order.getUser().getId()).get());
+        entity.setIncoming(order.getIncoming());
+        for (OrderLine line : order.getOrderList()) {
+            StorageEntity storage = new StorageEntity();
+            storage.setOrder(entity);
+            storage.setPhone(line.getPhone());
+            storage.setCount(line.getCount());
+            entity.getOrderList().add(storage);
         }
+        return entity;
+    }
+
+    public List<StorageLine> getStorageStock() {
+        List<StorageLine> storageLines = new ArrayList<>();
+        List<Object[]> list = storageRepository.find();
+        for (Object[] a : list) {
+            StorageLine line = new StorageLine();
+            BigInteger count = (BigInteger) a[0];
+            BigInteger id = (BigInteger) a[1];
+            line.setCount(count.longValue());
+            line.setPhone(phoneRepository.findById(id.longValue()).get());
+            storageLines.add(line);
+        }
+        return storageLines;
+
+    }
 }
