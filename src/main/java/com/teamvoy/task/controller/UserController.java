eaa8b29be3d690b4b46489348f2d988c9e39c4c0
alexandrmefisto <verjil159753>
2022-06-17 00:20:41 +0300

    added comments and code formatted

--- src/main/java/com/teamvoy/task/controller/UserController.java
+++ src/main/java/com/teamvoy/task/controller/UserController.java
@@ -10,8 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.websocket.server.PathParam;
-
+//REST controller for users
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -22,8 +21,8 @@ public class UserController {
 
     @PostMapping("/registration")
     public ResponseEntity registration(@RequestBody UserEntity user) {
+        // user registration
         try {
-
             userService.registration(user);
             return ResponseEntity.ok().body("User added successfully");
         } catch (UserAlreadyExistException e) {
@@ -35,6 +34,7 @@ public class UserController {
 
     @GetMapping("/storage")
     public ResponseEntity getStorageStock() {
+        // viewing storage stocks with actual prices
         try {
             return ResponseEntity.ok().body(managerService.getStorageStock());
         } catch (Exception e) {
@@ -43,23 +43,25 @@ public class UserController {
     }
 
     @PostMapping("/order")
-    public ResponseEntity incomingOrder(@RequestBody Order order) {
+    public ResponseEntity outgoingOrder(@RequestBody Order order) {
+        //order goods for users
         try {
             userService.outgoingOrder(order);
             return ResponseEntity.ok().body("Incoming order added successfully");
-        }catch (OutOfStockException e){
+        } catch (OutOfStockException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
+
     @PostMapping("/pay")
-    public ResponseEntity payOrder(@RequestParam Long order_id){
+    public ResponseEntity payOrder(@RequestParam Long order_id) {
+        //paying order for users
         try {
             userService.payOrder(order_id);
             return ResponseEntity.ok().body("Incoming order added successfully");
-        }catch (Exception e) {
+        } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
