90399f9434e8b85a6ff4cd02dd2a17297f9b885e
alexandrmefisto <verjil159753>
2022-06-16 23:56:47 +0300

    add payOrder

--- src/main/java/com/teamvoy/task/service/UserService.java
+++ src/main/java/com/teamvoy/task/service/UserService.java
@@ -3,32 +3,47 @@ package com.teamvoy.task.service;
 import com.teamvoy.task.entity.OrderEntity;
 import com.teamvoy.task.entity.StorageEntity;
 import com.teamvoy.task.entity.UserEntity;
+import com.teamvoy.task.exception.OutOfStockException;
 import com.teamvoy.task.exception.UserAlreadyExistException;
 import com.teamvoy.task.model.Order;
 import com.teamvoy.task.model.OrderLine;
-import com.teamvoy.task.repository.UserRepository;
+import com.teamvoy.task.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
 
 @Service
 public class UserService {
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private OrderRepository orderRepository;
+    @Autowired
+    private StorageRepository storageRepository;
+
     public UserEntity registration(UserEntity user) throws UserAlreadyExistException {
-        String  str = user.getUsername();
-        UserEntity userEntity = userRepository.findByUsername(str);
-        if (userEntity!=null){
+        String username = user.getUsername();
+        UserEntity userEntity = userRepository.findByUsername(username);
+        if (userEntity != null) {
             throw new UserAlreadyExistException("User already exist");
         }
         return userRepository.save(user);
     }
 
-    public void outgoingOrder(Order order) {
+    public void outgoingOrder(Order order) throws OutOfStockException {
         order.setIncoming(false);
+        order.calculateTotalCost();
+        inStock(order);
         OrderEntity entity = toEntityOrder(order);
+        IncomingOrderThread incomingOrderThread = new IncomingOrderThread(orderRepository, entity);
+        incomingOrderThread.start();
+
+//        orderRepository.save(entity);
     }
 
-    private OrderEntity toEntityOrder(Order order) {
+    public OrderEntity toEntityOrder(Order order) {
         //convert OrderModel to OrderEntity
         OrderEntity entity = new OrderEntity();
         entity.setUser(userRepository.findById(order.getUser().getId()).get());
@@ -42,5 +57,21 @@ public class UserService {
         }
         entity.setPaid(false);
         return entity;
+    }
+
+    public void inStock(Order order) throws OutOfStockException {
+        for (OrderLine line : order.getOrderList()) {
+            BigDecimal stock = storageRepository.getStockByPhone(line.getPhone().getId());
+            if (stock == null || (stock.longValue() - line.getCount()) < 0) {
+                throw new OutOfStockException(line.getPhone().getModel() + " is out of stock");
+            }
+        }
+
+    }
+
+    public void payOrder(Long order_id) {
+        OrderEntity order = orderRepository.findById(order_id).get();
+        order.setPaid(true);
+        orderRepository.save(order);
     }
 }
