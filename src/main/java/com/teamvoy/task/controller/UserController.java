70c8ddddb90da0291c4adfa55a47d6774eda3a2c
alexandrmefisto <verjil159753>
2022-06-16 18:58:37 +0300

    added getStorageStock

--- src/main/java/com/teamvoy/task/controller/UserController.java
+++ src/main/java/com/teamvoy/task/controller/UserController.java
@@ -2,6 +2,8 @@ package com.teamvoy.task.controller;
 
 import com.teamvoy.task.entity.UserEntity;
 import com.teamvoy.task.exception.UserAlreadyExistException;
+import com.teamvoy.task.model.Order;
+import com.teamvoy.task.service.ManagerService;
 import com.teamvoy.task.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
     @Autowired
     private UserService userService;
+    @Autowired
+    private ManagerService managerService;
     @PostMapping("/registration")
     public ResponseEntity registration(@RequestBody UserEntity user) {
         try {
@@ -24,7 +28,23 @@ public class UserController {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
-
+    @GetMapping("/storage")
+    public ResponseEntity getStorageStock(){
+        try {
+            return ResponseEntity.ok().body(managerService.getStorageStock());
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+    @PostMapping("/order")
+    public ResponseEntity incomingOrder(@RequestBody Order order) {
+        try {
+            userService.outgoingOrder(order);
+            return ResponseEntity.ok().body("Incoming order added successfully");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
 }
 
 
