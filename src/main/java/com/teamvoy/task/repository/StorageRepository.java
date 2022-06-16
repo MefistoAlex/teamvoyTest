1cd2bdf813051d4629301e8630e439201e864a25
alexandrmefisto <verjil159753>
2022-06-16 15:22:10 +0300

    correct query

--- src/main/java/com/teamvoy/task/repository/StorageRepository.java
+++ src/main/java/com/teamvoy/task/repository/StorageRepository.java
@@ -13,25 +13,18 @@ import java.util.List;
 public interface StorageRepository extends CrudRepository<StorageEntity,Long> {
 
 
-    @Query("select count (distinct s) from StorageEntity s where s.order.incoming = ?1")
-    long countDistinctByOrder_Incoming(Boolean incoming);
-
-    @Query("select s from StorageEntity s where s.phone.id = ?1")
-    List<StorageEntity> findByPhone_Id(Long id);
-
-    @Query("select s from StorageEntity s where s.order.incoming = ?1")
-    List<StorageEntity> findByOrder_Incoming(Boolean incoming);
-
-    @Query(value = "SELECT DISTINCT " +
-            "storage_entity.count AS count, " +
-            "storage_entity.phone_id AS id " +
-            "FROM " +
-            "storage_entity " +
-            "INNER JOIN " +
-            "phone_entity " +
-            "ON " +
-            "storage_entity.phone_id = phone_entity.id",nativeQuery = true)
-    List<Object[]> find();
+    @Query(value = "SELECT\n" +
+            "\tSUM(IF(incoming = true,storage_entity.count,-storage_entity.count)) as count, \n" +
+            "\tstorage_entity.phone_id\n" +
+            "FROM\n" +
+            "\tstorage_entity\n" +
+            "\tINNER JOIN\n" +
+            "\torder_entity\n" +
+            "\tON \n" +
+            "\t\tstorage_entity.order_id = order_entity.id\n" +
+            "GROUP BY\n" +
+            "\tstorage_entity.phone_id",nativeQuery = true)
+    List<Object[]> getGoodsStock();
 
 
 
