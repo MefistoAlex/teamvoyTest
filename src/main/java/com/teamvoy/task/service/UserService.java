d3ff1f2e45e904af9ceaa821373764fca95b8c5c
alexandrmefisto <verjil159753>
2022-06-16 19:00:05 +0300

    added outgoing order

--- src/main/java/com/teamvoy/task/service/UserService.java
+++ src/main/java/com/teamvoy/task/service/UserService.java
@@ -1,8 +1,11 @@
 package com.teamvoy.task.service;
 
+import com.teamvoy.task.entity.OrderEntity;
+import com.teamvoy.task.entity.StorageEntity;
 import com.teamvoy.task.entity.UserEntity;
 import com.teamvoy.task.exception.UserAlreadyExistException;
 import com.teamvoy.task.model.Order;
+import com.teamvoy.task.model.OrderLine;
 import com.teamvoy.task.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,5 +24,23 @@ public class UserService {
     }
 
     public void outgoingOrder(Order order) {
+        order.setIncoming(false);
+        OrderEntity entity = toEntityOrder(order);
+    }
+
+    private OrderEntity toEntityOrder(Order order) {
+        //convert OrderModel to OrderEntity
+        OrderEntity entity = new OrderEntity();
+        entity.setUser(userRepository.findById(order.getUser().getId()).get());
+        entity.setIncoming(order.getIncoming());
+        for (OrderLine line : order.getOrderList()) {
+            StorageEntity storage = new StorageEntity();
+            storage.setOrder(entity);
+            storage.setPhone(line.getPhone());
+            storage.setCount(line.getCount());
+            entity.getOrderList().add(storage);
+        }
+        entity.setPaid(false);
+        return entity;
     }
 }
