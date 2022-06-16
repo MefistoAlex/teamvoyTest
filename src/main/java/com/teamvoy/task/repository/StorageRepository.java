fc5265962ce9ef82f8ff2175cc9dbe50d904bfb4
alexandrmefisto <verjil159753>
2022-06-16 23:51:20 +0300

    add correct query for checking stocks by phone

--- src/main/java/com/teamvoy/task/repository/StorageRepository.java
+++ src/main/java/com/teamvoy/task/repository/StorageRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.Repository;
 
+import java.math.BigDecimal;
 import java.sql.ResultSet;
 import java.util.List;
 
@@ -14,7 +15,7 @@ public interface StorageRepository extends CrudRepository<StorageEntity,Long> {
 
 
     @Query(value = "SELECT\n" +
-            "\tSUM(IF(incoming = true,storage_entity.count,-storage_entity.count)) as count, \n" +
+            "\tsum(IF(incoming,count,-count)) as count, \n" +
             "\tstorage_entity.phone_id\n" +
             "FROM\n" +
             "\tstorage_entity\n" +
@@ -25,6 +26,18 @@ public interface StorageRepository extends CrudRepository<StorageEntity,Long> {
             "GROUP BY\n" +
             "\tstorage_entity.phone_id",nativeQuery = true)
     List<Object[]> getGoodsStock();
+
+    @Query(value = "SELECT\n" +
+            "\tSUM(IF(incoming,count,-count)) as count\n" +
+            "FROM\n" +
+            "\tstorage_entity\n" +
+            "\tINNER JOIN\n" +
+            "\torder_entity\n" +
+            "\tON \n" +
+            "\t\tstorage_entity.order_id = order_entity.id\n" +
+            "WHERE\n" +
+            "\tstorage_entity.phone_id = ?1",nativeQuery = true)
+    BigDecimal getStockByPhone(Long phoneId);
 
 
 
