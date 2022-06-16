33fa4ada3c33b22061ca085cc2c99fe200808b41
alexandrmefisto <verjil159753>
2022-06-16 15:22:25 +0300

    correct query

--- src/main/java/com/teamvoy/task/repository/PriseRepository.java
+++ src/main/java/com/teamvoy/task/repository/PriseRepository.java
@@ -2,8 +2,11 @@ package com.teamvoy.task.repository;
 
 import com.teamvoy.task.entity.PhoneEntity;
 import com.teamvoy.task.entity.PriceEntity;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PriseRepository extends CrudRepository<PriceEntity,Long> {
-    PriceEntity findTopByPhone(PhoneEntity phone);
+
+    PriceEntity findTopByPhoneId(Long id);
+
 }
