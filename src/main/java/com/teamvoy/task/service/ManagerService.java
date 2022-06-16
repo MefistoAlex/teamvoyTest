0edf15cf913bfad85058ab85a4dff05fbcc44ed5
alexandrmefisto <verjil159753>
2022-06-16 15:23:09 +0300

    deleting comments

--- src/main/java/com/teamvoy/task/service/ManagerService.java
+++ src/main/java/com/teamvoy/task/service/ManagerService.java
@@ -11,6 +11,7 @@ import com.teamvoy.task.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
@@ -34,13 +35,6 @@ public class ManagerService {
     }
 
     public OrderEntity incomingOrder(Order order) {
-        //test order
-        /* order.setIncoming(true);
-        order.setUser(User.toModel(userRepository.findById(2L).get()));
-        OrderLine orderLine = new OrderLine(phoneRepository.findById(3L).get(),4L,4.12D );
-        OrderLine orderLine2 = new OrderLine(phoneRepository.findById(4L).get(),13L,23.12D );
-        order.getOrderList().add(orderLine);
-        order.getOrderList().add(orderLine2);*/
         OrderEntity entity = toEntityOrder(order);
         priceRepository.saveAll(getPriceList(order));
         return orderRepository.save(entity);
@@ -78,32 +72,17 @@ public class ManagerService {
         List<Object[]> list = storageRepository.getGoodsStock();
         for (Object[] a : list) {
             StorageLine line = new StorageLine();
-            BigInteger count = (BigInteger) a[0];
+            BigDecimal count = (BigDecimal) a[0];
             BigInteger id = (BigInteger) a[1];
             line.setCount(count.longValue());
             line.setPhone(phoneRepository.findById(id.longValue()).get());
-            line.setPrice(priceRepository.findTopByPhone(line.getPhone()).getPrice());
+            line.setPrice(priceRepository.findTopByPhoneId(line.getPhone().getId()).getPrice());
             storageLines.add(line);
         }
         return storageLines;
     }
 
     public List<PriceEntity> setPrices(List<PriceEntity> priceList) {
-//        priceList = new ArrayList<>();
-//        PriceEntity entity1 = new PriceEntity();
-//        entity1.setPhone(phoneRepository.findById(3L).get());
-//        entity1.setPrice(10D);
-//        PriceEntity entity = new PriceEntity();
-//        entity.setPhone(phoneRepository.findById(4L).get());
-//        entity.setPrice(20D);
-//        PriceEntity entity2 = new PriceEntity();
-//        entity2.setPhone(phoneRepository.findById(5L).get());
-//        entity2.setPrice(30D);
-//
-//        priceList.add(entity1);
-//        priceList.add(entity);
-//        priceList.add(entity2);
-
        return (List<PriceEntity>) priceRepository.saveAll(priceList);
     }
 }
